b49283382c5926d32cb437a9e7816171fd8e71f0
der1x <130235333+Mark4353@users.noreply.github.com>
2025-11-16 13:25:17 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./reset.css";
 import Header from "./components/header/Header.jsx";
 import Hero from "./components/hero/Hero.jsx";
 import Main from "./components/main/Main.jsx";
-import Slider from "./components/slider/Slider.jsx";
 import Footer from "./components/footer/Footer.jsx";
 function App() {
   return (
